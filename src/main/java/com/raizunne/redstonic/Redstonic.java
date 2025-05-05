47d689235e6a79cadb0d4fdae83f8487be91e612
Lu <akitomoya2908@gmail.com>
2025-05-05 09:48:33 +0700

    commit

--- src/main/java/com/raizunne/redstonic/Redstonic.java
+++ src/main/java/com/raizunne/redstonic/Redstonic.java
@@ -43,7 +43,7 @@ import static com.raizunne.redstonic.Handler.RedstonicCommands.*;
 public class Redstonic {
 
     public static final String MODID = "Redstonic";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = Tags.VERSION;
     public static final String NAME = "Redstonic";
 
     @Mod.Instance
